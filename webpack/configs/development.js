581f485ec3941671a4dfead6ef3d2d2ed9ce9c6d
salikovskiy <ssalikovskiy@gmail.com>
2019-12-04 12:16:18 +0200

    webpack init

--- webpack/configs/development.js
+++ webpack/configs/development.js
@@ -16,7 +16,7 @@ module.exports = env => ({
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: '../src/public/index.html',
     }),
   ],
   devServer: {

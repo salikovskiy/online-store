a34128ca9cfa064cc84d4b31a1eec69f0ba8aace
Vanya <marisvetik@gmail.com>
2019-12-05 12:32:52 +0200

    before merge

--- src/services/services.js
+++ src/services/services.js
@@ -1,1 +1,14 @@
-export default {};
+// export default {};
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  arcticleList: document.querySelector('.gallery'),
+};
+
+refs.searchForm('submit', searchFormSubmitHandler);
+
+function searchFormSubmitHandler(e) {
+  e.preventDefault();
+
+  console.log(e.currentTarget);
+}

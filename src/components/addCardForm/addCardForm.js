9c60ec83023c3c3fbb923f0e29b9bc9c6e20c44a
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2019-12-06 16:50:57 +0200

    form addcard editing

--- src/components/addCardForm/addCardForm.js
+++ src/components/addCardForm/addCardForm.js
@@ -9,59 +9,63 @@ import API from '../../services/services.js';
 
 const modalForm = document.querySelector('.modalForm');
 const openModalForm = document.querySelector('.create-ad');
+const category = {
+  name: [],
+};
+
+function getData() {
+  return API.getAllProduct().then(data => {
+    data.categories.forEach(item => {
+      category.name = [...category.name, item.category];
+    });
+    return category.name;
+  });
+}
 
 const formMarkup = `
 <div class="modalBox">
 <div class="formWrapper">
 <button type="button" class="closeForm" data-action="closeForm">
-    <i class="material-icons">close</i>
+<i class="material-icons">close</i>
 </button>
 <form class="form">
 
 <p class="formTitle">Додати оголошення</p>
 <div class="firstInput__Wrapper iw">
-    <label for="firstInput" class="firstInput__label">Назва товару</label>
-    <input class="firstInput" type="text" name="title" />
+<label for="firstInput" class="firstInput__label">Назва товару</label>
+<input class="firstInput" type="text" name="title" />
 </div>
 <div class="secondInput__Wrapper iw">
-    <p class="secondInput--title">Фото</p>
-    <div class="formGroup">
-        <label class="secondLabel">
-            <span class="plusPhoto">+</span>
-            <span class="plusPhoto"></span>
-            <span class="plusPhoto"></span>
-            <span class="plusPhoto"></span>
-            <span class="plusPhoto"></span>
-            <span class="plusPhoto"></span>
-            <input type="file" name="images" class="fileInput" />
-        </label>
-    </div>
+<p class="secondInput--title">Фото</p>
+<div class="formGroup">
+<label class="secondLabel">
+<span class="plusPhoto">+</span>
+<span class="plusPhoto"></span>
+<span class="plusPhoto"></span>
+<span class="plusPhoto"></span>
+<span class="plusPhoto"></span>
+<span class="plusPhoto"></span>
+<input type="file" name="images" class="fileInput" />
+</label>
+</div>
 </div>
 <div class="thirdInput__Wrapper iw">
-    <label for="thirdInput" class="thirdInput__label">Опис товару</label>
-    <input class="thirdInput" type="text" name="description" />
+<label for="thirdInput" class="thirdInput__label">Опис товару</label>
+<input class="thirdInput" type="text" name="description" />
 </div>
 <div class="selectWrapper iw">
-    <label for="select">Категорія товару</label>
-    <select class="categorySelect" name="category">
-    <option></option>
-        <option id="1">Нерухомість</option>
-        <option id="2">Транспорт</option>
-        <option id="3">Робота</option>
-        <option id="4">Електроніка</option>
-        <option id="5">Бізнес та послуги</option>
-        <option id="6">Відпочинок і спорт</option>
-        <option id="7">Віддам безкоштовно</option>
-        <option id="8">Обмін</option>
-    </select>
+<label for="select">Категорія товару</label>
+<select class="categorySelect" name="category">
+<option></option>
+</select>
 </div>
 <div class="fourthInput__Wrapper iw">
-    <label class="fourthInput__label" for="fourthInput">Ціна</label>
-    <input class="fourthInput" type="number" name="price" />
+<label class="fourthInput__label" for="fourthInput">Ціна</label>
+<input class="fourthInput" type="number" name="price" />
 </div>
 <div class="firthInput__Wrapper iw">
-    <label for="fifthInput" class="fifthInput__label">Телефон</label>
-    <input class="fifthInput" type="tel" name="phone" />
+<label for="fifthInput" class="fifthInput__label">Телефон</label>
+<input class="fifthInput" type="tel" name="phone" />
 </div>
 <button type="submit" class="addButton">Додати
 </button>
@@ -69,13 +73,19 @@ const formMarkup = `
 </div>
 </div>`;
 
-// ------------Рендер формы по клику-------------
+//--------------Рендер формы по клику---------------
 
 openModalForm.addEventListener('click', e => {
   e.preventDefault();
   modalForm.insertAdjacentHTML('beforeend', formMarkup);
+  const select = document.querySelector('.categorySelect');
+  getData().then(data => {
+    let string = '';
+    data.forEach(item => (string += `<option>${item}</option>`));
+    select.insertAdjacentHTML('beforeend', string);
+  });
 
-  //   ----------Закртытие формы по клику (поставил слушателя внутрь слушателя, потому что второй слушатель не находит форму, так как она динамически отрисовывается)
+  //-------------Закртытие формы по клику-----------
 
   const modalBox = document.querySelector('.modalBox');
   const form = document.querySelector('.form');
@@ -90,19 +100,18 @@ openModalForm.addEventListener('click', e => {
     }
   });
 
-  // -------------Собираю данные из формы-------------
+  //-------------Собираю данные из формы-------------
 
   form.addEventListener('submit', formHandleSubmit);
   function formHandleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const object = {};
+    const data = {};
     formData.forEach((value, name) => {
-      object[name] = value;
-
-      // ---------------Отправляю данные на сервер-----------
-
-      API.adsProduct(object);
+      data[name] = value;
     });
+    console.log('Объект данных из формы:', data);
+
+    //----------Отправляю данные на сервер-----------
   }
 });

2ee14552ca452ec1ae25230509f4dd2bb47d21ce
salikovskiy <ssalikovskiy@gmail.com>
2019-12-14 18:23:15 +0200

    beginning

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,42 +1,42 @@
-import axios from 'axios';
-import categoriesFunc from '../categories/categories';
-const categories = document.querySelector('.categories');
-import itemCard from '../itemCard/itemCard';
+// import axios from 'axios';
+// import categoriesFunc from '../categories/categories';
+// const categories = document.querySelector('.categories');
+// import itemCard from '../itemCard/itemCard';
 
-async function getInfo() {
-  return await axios.get(
-    `https://dashads.goit.co.ua/api/v1/ads/all?category=2&page=1`,
-  );
-}
+// async function getInfo() {
+//   return await axios.get(
+//     `https://dashads.goit.co.ua/api/v1/ads/all?category=2&page=1`,
+//   );
+// }
 
-let card = '';
+// let card = '';
 
-const state = {
-  currentImage: '',
-  currentCategory: null,
-  allAds: [],
-  prevImage: null,
-  nextImage: null,
-};
+// const state = {
+//   currentImage: '',
+//   currentCategory: null,
+//   allAds: [],
+//   prevImage: null,
+//   nextImage: null,
+// };
 
-let small = 0;
-let big = 3;
+// let small = 0;
+// let big = 3;
 
-categories.addEventListener('click', e => {
-  if (e.target.classList.contains('categories-item-btn-slider-prev')) {
-    const ul = e.target.parentNode.closest('li').lastElementChild;
-  }
-  if (e.target.classList.contains('categories-item-btn-slider-next')) {
-    const ul = e.target.parentNode.closest('li').lastElementChild;
-    small++;
-    big++;
-    console.log(small, big);
-    card = '';
-    data.forEach((item, index) => {
-      if (index >= small && index <= big) {
-        card += `<li class="listcards-itemcard">${itemCard(item)}</li>`;
-      }
-      ul.innerHTML = card;
-    });
-  }
-});
+// categories.addEventListener('click', e => {
+//   if (e.target.classList.contains('categories-item-btn-slider-prev')) {
+//     const ul = e.target.parentNode.closest('li').lastElementChild;
+//   }
+//   if (e.target.classList.contains('categories-item-btn-slider-next')) {
+//     const ul = e.target.parentNode.closest('li').lastElementChild;
+//     small++;
+//     big++;
+//     console.log(small, big);
+//     card = '';
+//     data.forEach((item, index) => {
+//       if (index >= small && index <= big) {
+//         card += `<li class="listcards-itemcard">${itemCard(item)}</li>`;
+//       }
+//       ul.innerHTML = card;
+//     });
+//   }
+// });
